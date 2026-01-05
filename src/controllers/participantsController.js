8cf391e46aa0f8f86c58a390904168a3a3cfedc8
MD Alquama Jia <mdalquamajia@gmail.com>
2026-01-05 12:35:30 +0530

    participants controller updated

--- src/controllers/participantsController.js
+++ src/controllers/participantsController.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const participantsService = require("../services/participantsService");
 const meetingsService = require("../services/meetingsService");
 const BusinessException = require("../exceptions/businessException");
@@ -110,57 +109,5 @@ exports.kickParticipant = async (req, res, next) => {
     if (err instanceof BusinessException)
       return res.status(err.status).json({ error: err.message });
     next(err);
-=======
-const participantService = require("../services/participantService");
-const {
-  createParticipantSchema,
-  updateParticipantSchema,
-} = require("../validations/participantValidation");
-
-exports.create = async (req, res) => {
-  try {
-    const { error } = createParticipantSchema.validate(req.body);
-    if (error)
-      return res.status(400).json({ error: error.details[0].message });
-
-    const participant = await participantService.createParticipant(
-      req.body,
-      req.user.id
-    );
-
-    res.status(201).json({ success: true, participant });
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-};
-
-exports.getAllByMeeting = async (req, res) => {
-  try {
-    const participants =
-      await participantService.getParticipantsByMeeting(
-        req.params.meetingId
-      );
-
-    res.json({ success: true, participants });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-exports.update = async (req, res) => {
-  try {
-    const { error } = updateParticipantSchema.validate(req.body);
-    if (error)
-      return res.status(400).json({ error: error.details[0].message });
-
-    const participant = await participantService.updateParticipant(
-      req.params.id,
-      req.body
-    );
-
-    res.json({ success: true, participant });
-  } catch (err) {
-    res.status(400).json({ error: err.message });
->>>>>>> origin
   }
 };
